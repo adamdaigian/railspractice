a129ffcacec7b3795a4e14da2ae1e772afbb70aa
adamdaigian <adam.daigian@gmail.com>
2013-10-20 13:17:14 -0700

    new rails app to practice auth with github, haml and testing

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # attr_accessor :firstname, :lastname, :email, :password
+
+
+  # validates_legnth_of :password, :minimum => 6, :message => "password must be at least 6 chracters long"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practice::Application.config.secret_key_base = '9cb82669bf04c635c8ee19930bd5bcfbfeded9ccbc79f145b71d2866b84ff7ed83a2fd1684b45bdb569432087ef85bf57ce44c2a8ac3c977bf2f6c4844c3f9a2'
